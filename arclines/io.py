ce55bb0e61e1d350eadc08ae0a6e185f120f24a9
J. Xavier Prochaska <xavier@ucolick.org>
2016-12-17 11:00:40 -0800

    loading

--- arclines/io.py
+++ arclines/io.py
@@ -2,13 +2,18 @@
 """
 from __future__ import (print_function, absolute_import, division, unicode_literals)
 
+import numpy as np
+import os
 import datetime
+import pdb
 
-from astropy.table import Table
+from astropy.table import Table, vstack
 
+from arclines import defs
 
 def load_line_list(line_file, add_path=False):
-    """
+    """ Simple load of a line list
+
     Parameters
     ----------
     line_file : str
@@ -24,6 +29,45 @@ def load_line_list(line_file, add_path=False):
     # Return
     return line_list
 
+
+def load_line_lists(lines, unknown=False):
+    """ Loads a series of line list files
+
+    Parameters
+    ----------
+    lamps : list
+    unknown : bool, optional
+
+    Returns
+    -------
+    line_list : Table
+
+    """
+    import arclines # For path
+    line_path = arclines.__path__[0]+'/data/lists/'
+
+    # Read standard files
+    lists = []
+    for line in lines:
+        line_file = line_path+'{:s}_lines.dat'.format(line)
+        if not os.path.isfile(line_file):
+            raise IOError("Input line {:s} is not included in arclines".format(line))
+        else:
+            lists.append(load_line_list(line_file))
+    # Stack
+    line_lists = vstack(lists, join_type='exact')
+
+    # Unknown
+    if unknown:
+        unkn_lines = load_unknown_list(lines)
+        unkn_lines.remove_column('line_flag')  # may wish to have this info
+        # Stack
+        line_lists = vstack([line_lists, unkn_lines])
+
+    # Return
+    return line_lists
+
+
 def load_source_table():
     """ Load table of arcline sources
 
@@ -38,6 +82,36 @@ def load_source_table():
     sources = Table.read(src_file, format='ascii.fixed_width', comment='#')
     # Return
     return sources
+
+
+def load_unknown_list(lines, unknwn_file=None):
+    """
+    Parameters
+    ----------
+    lines : list
+    unknwn_file : str, optional
+
+    Returns
+    -------
+    unknwn_lines : Table
+
+    """
+    import arclines # For path
+    line_dict = defs.lines()
+    # Load
+    line_path = arclines.__path__[0]+'/data/lists/'
+    if unknwn_file is None:
+        unknwn_file = line_path+'UNKNWN_lines.dat'
+    # Cut on input lamps
+    line_list = load_line_list(unknwn_file)
+    msk = np.array([False]*len(line_list))
+    for line in lines:
+        line_flag = line_dict[line]
+        match = line_list['line_flag'] % 2*line_flag >= line_flag
+        msk[match] = True
+        pdb.set_trace()
+    # Finish
+    return line_list[msk]
 
 
 def write_line_list(tbl, outfile):
--- arclines/tests/test_io.py
+++ arclines/tests/test_io.py
@@ -14,6 +14,15 @@ from arclines import io as arcl_io
 #    return os.path.join(data_dir, filename)
 
 
+def test_load_line_lists():
+    line_lists = arcl_io.load_line_lists(['HgI','ZnI'])
+    pytest.set_trace()
+
+def test_load_line_list():
+    import arclines
+    llst = arcl_io.load_line_list(arclines.__path__[0]+'/data/lists/HgI_lines.dat')
+
+
 def test_load_sources():
     sources = arcl_io.load_source_table()
     # Test
