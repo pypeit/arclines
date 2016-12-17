8f8b723692136bc4b4caadb3757b8ce9c368ae03
J. Xavier Prochaska <xavier@ucolick.org>
2016-12-17 10:41:31 -0800

    starting the holy war

---
+++ arclines/holy/patterns.py
@@ -0,0 +1,8 @@
+""" Module for finding patterns in arc line spectra
+"""
+from __future__ import (print_function, absolute_import, division, unicode_literals)
+
+from astropy.table import Table
+
+
+
